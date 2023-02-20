2040f143846273f569f105b8e27ee0b6bd2ffba5
Roxanne Alvidrez Niederkorn <rox.niederkorn@gmail.com>
2023-02-19 19:27:35 -0600

    GET request made to select genres for each movie

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -16,5 +16,23 @@ router.get('/', (req, res) => {
 
 });
 
+router.get('/:id', (req, res) => {
+  console.log('req.params.id:', req.params.id);
+  // query to join relations between genres and movies
+  const queryText = `SELECT "genres"."name" FROM "genres"
+  JOIN "movies_genres" ON "genres"."id" = "movies_genres"."genre_id"
+  JOIN "movies" ON "movies_genres"."movie_id" = "movies"."id"
+  WHERE "movies"."id" = $1;`;
+  pool.query(queryText, [req.params.id])
+  .then((response) => {
+    console.log('response.rows:', response.rows);
+    res.send(response.rows);
+  }).catch((error) => {
+    console.log('error in genre.router GET request', error);
+    res.sendStatus(500);
+  });
+});
+
 
 module.exports = router;
+
