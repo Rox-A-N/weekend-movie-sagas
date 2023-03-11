50bc97cec21f809f08e11001c8a9d2dcc7b1fbd1
Roxanne Alvidrez Niederkorn <rox.niederkorn@gmail.com>
2023-03-11 02:21:01 -0600

    standardized query const

--- server/routes/movie.router.js
+++ server/routes/movie.router.js
@@ -18,14 +18,14 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   const idToGet = req.params.id;
-  const sqlText = `SELECT * FROM movies WHERE id=$1`;
-  pool.query(sqlText, [idToGet])
+  const query = `SELECT * FROM movies WHERE id=$1`;
+  pool.query(query, [idToGet])
   .then( (result) => {
     console.log(`Movie with id ${idToGet}`, result.rows);
     res.send(result.rows);
   })
   .catch( (error) => {
-    console.log(`Error making database query ${sqlText}`, error);
+    console.log(`Error making database query ${query}`, error);
     res.sendStatus(500);
   })
 });
